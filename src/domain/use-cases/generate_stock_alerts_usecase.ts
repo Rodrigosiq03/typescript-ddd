6c9ddc589d0e0a179504a61e090c2f0bf4427d34
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-06-27 13:43:59 -0300

    Adiciona a classe GenerateStockAlertsUseCase para gerar alertas de estoque com base na quantidade de produtos e no mínimo necessário, integrando repositórios de produtos e alertas.

---
+++ src/domain/use-cases/generate_stock_alerts_usecase.ts
@@ -0,0 +1,32 @@
+import { IProductRepository } from "../repositories/product_repository_interface";
+import { IAlertRepository } from "../repositories/alert_repository_interface";
+import { Alert } from "../entities/alert";
+import { UniqueEntityID } from "../../core/entities/unique_entity_id";
+
+export class GenerateStockAlertsUseCase {
+  constructor(
+    private productRepo: IProductRepository,
+    private alertRepo: IAlertRepository
+  ) {}
+
+  async execute(): Promise<Alert[]> {
+    const products = await this.productRepo.getAll();
+
+    for (const product of products) {
+      if (product.quantity <= product.minimumQuantity) {
+        const alert = new Alert({
+          productId: new UniqueEntityID(product.id.toString()),
+          alertDate: new Date(),
+          method: "EMAIL",
+        });
+
+        console.log(`Generating alert for product ${product.id} - ${product.name}`);
+
+        await this.alertRepo.create(alert);
+      }
+    }
+
+    const alerts = await this.alertRepo.getAll();
+    return alerts
+  }
+}
