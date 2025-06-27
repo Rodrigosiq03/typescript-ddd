f819b7e515706850c9736f7a3fa2dd2030ea821f
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-06-27 13:43:37 -0300

    Adiciona método getAlertsByProductId à interface IAlertRepository para recuperar alertas por ID de produto.

--- src/domain/repositories/alert_repository_interface.ts
+++ src/domain/repositories/alert_repository_interface.ts
@@ -4,4 +4,5 @@ export interface IAlertRepository {
   getById(id: string): Promise<Alert | undefined>;
   getAll(): Promise<Alert[]>;
   create(alert: Alert): Promise<Alert>;
+  getAlertsByProductId(productId: string): Promise<Alert[]>;
 }
