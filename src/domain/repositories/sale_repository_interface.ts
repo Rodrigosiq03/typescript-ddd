1d68d25c12377bf4af813344015ebdcf12e43730
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-06-27 13:43:52 -0300

    Substitui o método getAll por getByProductId na interface ISaleRepository para recuperar vendas por ID de produto.

--- src/domain/repositories/sale_repository_interface.ts
+++ src/domain/repositories/sale_repository_interface.ts
@@ -2,6 +2,6 @@ import { Sale } from "../entities/sale";
 
 export interface ISaleRepository {
   getById(id: string): Promise<Sale | undefined>;
-  getAll(): Promise<Sale[]>;
+  getByProductId(productId: string): Promise<Sale[]>;
   create(sale: Sale): Promise<Sale>;
 }
